a25b9eb9a76c7607e24b87de0edec2374fc6a8c1
CaptainLDawg <Logan.Kawiti@hotmail.com>
2016-03-16 10:03:45 +1300

    Hangman App
    Hangman application for Assessment 5 at Vision College

---
+++ PaperHangMan/PaperHangMan/DatabaseManager.cs
@@ -0,0 +1,74 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace PaperHangMan
+{
+    public class DatabaseManager
+    {
+        static string dbName = "tblWords.sqlite";
+        string dbPath = System.IO.Path.Combine(Android.OS.Environment.ExternalStorageDirectory.ToString(), dbName);
+
+        static string dbNameLeader = "LeaderboardHang.sqlite";
+        string dbPathLeader = System.IO.Path.Combine(Android.OS.Environment.ExternalStorageDirectory.ToString(), dbNameLeader);
+
+        public DatabaseManager()
+        {
+
+        }
+        public List<ListONames> ViewAllWords()
+        {
+            try
+            {
+                using (var conn = new SQLite.SQLiteConnection(dbPath))
+                {
+                    var cmd = new SQLite.SQLiteCommand(conn);
+                    cmd.CommandText = "Select * from tbl_Words";
+                    var WordList = cmd.ExecuteQuery<ListONames>();
+                    return WordList;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error:" + e.Message);
+                return null;
+            }
+        }
+        public void AddLeaderboard(string name, int score, int letters)
+        {
+            try
+            {
+                using (var conn = new SQLite.SQLiteConnection(dbPathLeader))
+                {
+                    var cmd = new SQLite.SQLiteCommand(conn);
+                    cmd.CommandText = "insert into tblHangmanLeaderboard(HangName,HangScore, HangLetterAmt) values('" + name + "','" + score + "','" + letters + "')";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error:" + e.Message);
+            }
+        }
+        public List<ListOScores> ViewLeaderboard()
+        {
+            try
+            {
+                using (var conn = new SQLite.SQLiteConnection(dbPathLeader))
+                {
+                    var cmd = new SQLite.SQLiteCommand(conn);
+                    cmd.CommandText = "Select * from tblHangmanLeaderboard ORDER BY HangScore DESC;";
+                    var LeaderboardList = cmd.ExecuteQuery<ListOScores>();
+                    return LeaderboardList;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error:" + e.Message);
+                return null;
+            }
+        }
+    }
+}
